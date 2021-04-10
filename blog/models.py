1f6dbbc9bd0caac0fbbe64870c67b493f8d7a56f
Lohana Torres <lt.leopoldo@gmail.com>
2021-04-10 20:24:01 -0300

    feat: add complexity on post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -5,7 +7,10 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
   title = models.CharField(max_length=255)
-  resume = models.CharField(max_length=255)
-  content = models.TextField()
+  summary = RichTextField(max_length=255)
+  content = RichTextUploadingField()
   author = models.ForeignKey(User, on_delete=models.PROTECT)
   created_at = models.DateField(auto_now_add=True)
+
+  def __str__(self):
+    return self.title
